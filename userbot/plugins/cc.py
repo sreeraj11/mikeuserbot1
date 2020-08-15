7e6cdc75b183faf3d7cc0a363c2e1c3eb35a9937
sreeraj11 <51787957+sreeraj11@users.noreply.github.com>
2020-08-15 11:39:08 +0530

    Update cc.py

--- userbot/plugins/cc.py
+++ userbot/plugins/cc.py
@@ -1,13 +1,20 @@
 import asyncio
 from telethon import events
+import requests
 from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 
-@borg.on(admin_cmd(pattern="cc ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-         return
-    reply_text = "hi"
-    await event.edit(reply_text) 
 
+
+@borg.on(admin_cmd(pattern="cc ?(.*)"))
+
+async def payf(event):
+    input_str=event.pattern_match.group(100)
+    if input_str:
+        paytext=input_str
+        await event.edit(paytext)
+    else:
+        pay = "╭━━━╮\n┃╭━━╯\n┃╰━━╮\n┃╭━━╯\n┃┃\n╰╯\n"
+   # pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*8, paytext*8, paytext*2, paytext*2, paytext*2, paytext*6, paytext*6, paytext*2, paytext*2, paytext*2, paytext*2, paytext*2)
+    await event.edit(pay)
